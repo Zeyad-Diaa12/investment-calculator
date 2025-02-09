ed3b226c4c4a602c1a32af953adf322575767bcc
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-02-09 22:27:16 +0200

    migrate to modules

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
 import { InvestmentCalculatorService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
