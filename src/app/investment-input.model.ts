279822ea9a67baae07b840e2ed923ffa10cdbc7a
Zeyad-Diaa12 <zeyaddiaa2@gmail.com>
2025-02-09 21:44:08 +0200

    investment calculation

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,6 @@
+export interface Investmentinput {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Investmentinput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '10';
+  duration = '10';
+  @Output() calculate = new EventEmitter<Investmentinput>();
 
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: parseFloat(this.initialInvestment),
+      annualInvestment: parseFloat(this.annualInvestment),
+      expectedReturn: parseFloat(this.expectedReturn),
+      duration: parseInt(this.duration)
+    });
+  }
 }
